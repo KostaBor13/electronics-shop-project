646f597239aa1ea245d715db60ada0431e659cd3
Kostas <zaikin399@mail.ru>
2024-02-09 15:45:15 +0300

    исправление дз часть 2

--- src/phone.py
+++ src/phone.py
@@ -18,7 +18,7 @@ class Phone(Item):
     @number_of_sim.setter
     def number_of_sim(self, value):
         """Проверка условия значения на 0 и целое число"""
-        if not isinstance(value, int) or value == 0:
+        if not isinstance(value, int) or value <= 0:
             print("ValueError: Количество физических SIM-карт должно быть целым числом больше нуля")
         else:
             self._number_of_sim = value
